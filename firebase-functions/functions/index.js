a2d4889b284d61f1e151de5536e22cd3ced58429
Chris Styles <christopherstyles@users.noreply.github.com>
2020-08-19 23:07:41 -0700

    Update metadataRef assignment

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -39,7 +39,7 @@ exports.processSignUp = functions.auth.user().onCreate((user) => {
     .setCustomUserClaims(user.uid, customClaims)
     .then(() => {
       // Update real-time database to notify client to force refresh.
-      // const metadataRef = admin.database().ref('metadata/' + user.uid);
+      const metadataRef = admin.database().ref('metadata/' + user.uid);
       // Set the refresh time to the current UTC timestamp.
       // This will be captured on the client to force a token refresh.
       return metadataRef.set({ refreshTime: new Date().getTime() });
